6f6efe480bfb2a292c097294acd7fc47cb1ba2fb
Dan O'Connor <dan_o'connor@us.ibm.com>
2016-07-11 17:16:27 -0400

    update for ga

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function updateMessage(response) {
     // Depending on the confidence of the response the app can return different messages.
     // The confidence will vary depending on how well the system is trained. The service will always try to assign
     // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intentgit . In these cases it is usually best to return a disambiguation message
+    // user's intent . In these cases it is usually best to return a disambiguation message
     // ('I did not understand your intent, please rephrase your question', etc..)
     if (intent.confidence >= 0.75) {
       responseText = 'I understood your intent was ' + intent.intent;
