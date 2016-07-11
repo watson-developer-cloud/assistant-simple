0da2ec2f7c5a87d153932e62cd81ee5cda7e5f51
Dan O'Connor <dan_o'connor@us.ibm.com>
2016-07-11 18:39:51 -0400

    Add dlg response (#21)
    * prepping for dialog
    
    * prepping for dialog
    
    * prepping for dialog
    
    * updated to use free plan
    
    * update for ga
    
    * update for ga
    
    * update sdk

--- app.js
+++ app.js
@@ -30,11 +30,11 @@ app.use(bodyParser.json());
 
 // Create the service wrapper
 var conversation = watson.conversation({
-  url: 'https://gateway.watsonplatform.net/conversation-experimental/api',
+  url: 'https://gateway.watsonplatform.net/conversation/api',
   username: process.env.CONVERSATION_USERNAME || '<username>',
   password: process.env.CONVERSATION_PASSWORD || '<password>',
-  version_date: '2016-05-19',
-  version: 'v1-experimental'
+  version_date: '2016-07-11',
+  version: 'v1'
 });
 
 // Endpoint to be call from the client side
@@ -75,15 +75,17 @@ app.post('/api/message', function(req, res) {
  */
 function updateMessage(response) {
   var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
   if (response.intents && response.intents[0]) {
     var intent = response.intents[0];
-    if (!response.output) {
-      response.output = {};
-    }
     // Depending on the confidence of the response the app can return different messages.
     // The confidence will vary depending on how well the system is trained. The service will always try to assign
     // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent. In these cases it is usually best to return a disambiguation message
+    // user's intent . In these cases it is usually best to return a disambiguation message
     // ('I did not understand your intent, please rephrase your question', etc..)
     if (intent.confidence >= 0.75) {
       responseText = 'I understood your intent was ' + intent.intent;
