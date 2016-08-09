e1fecc11d8c36c1c2571ff48b57f08950236a2ec
ColbyJohnIBM <colbyj@us.ibm.com>
2016-08-09 12:31:41 -0400

    Fix underlining with regard to responsiveness / screen resizing (#43)

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -45,11 +45,19 @@ var ConversationPanel = (function() {
     };
   }
 
+// Set up the input box to underline text as it is typed
+  // This is done by creating a hidden dummy version of the input box that
+  // is used to determine what the width of the input text should be.
+  // This value is then used to set the new width of the visible input box.
   function setupInputBox() {
     var input = document.getElementById('textInput');
     var dummy = document.getElementById('textInputDummy');
-    var padding = 3;
+    var minFontSize = 14;
+    var maxFontSize = 16;
+    var minPadding = 4;
+    var maxPadding = 6;
 
+    // If no dummy input box exists, create one
     if (dummy === null) {
       var dummyJson = {
         'tagName': 'div',
@@ -60,28 +68,47 @@ var ConversationPanel = (function() {
       };
 
       dummy = Common.buildDomElement(dummyJson);
-      ['font-size', 'font-style', 'font-weight', 'font-family', 'line-height', 'text-transform', 'letter-spacing'].forEach(function(index) {
-        dummy.style[index] = window.getComputedStyle( input, null ).getPropertyValue( index );
-      });
-
       document.body.appendChild(dummy);
     }
 
-    input.addEventListener('input', function() {
-      if (this.value === '') {
-        this.classList.remove('underline');
-        this.setAttribute('style', 'width:' + '100%');
-        this.style.width = '100%';
+    function adjustInput() {
+      if (input.value === '') {
+        // If the input box is empty, remove the underline
+        input.classList.remove('underline');
+        input.setAttribute('style', 'width:' + '100%');
+        input.style.width = '100%';
       } else {
-        this.classList.add('underline');
-        var txtNode = document.createTextNode(this.value);
+        // otherwise, adjust the dummy text to match, and then set the width of
+        // the visible input box to match it (thus extending the underline)
+        input.classList.add('underline');
+        var txtNode = document.createTextNode(input.value);
+        ['font-size', 'font-style', 'font-weight', 'font-family', 'line-height',
+          'text-transform', 'letter-spacing'].forEach(function(index) {
+            dummy.style[index] = window.getComputedStyle(input, null).getPropertyValue(index);
+          });
         dummy.textContent = txtNode.textContent;
-        var widthValue = ( dummy.offsetWidth + padding) + 'px';
-        this.setAttribute('style', 'width:' + widthValue);
-        this.style.width = widthValue;
-      }
-    });
 
+        var padding = 0;
+        var htmlElem = document.getElementsByTagName('html')[0];
+        var currentFontSize = parseInt(window.getComputedStyle(htmlElem, null).getPropertyValue('font-size'), 10);
+        if (currentFontSize) {
+          padding = Math.floor((currentFontSize - minFontSize) / (maxFontSize - minFontSize)
+            * (maxPadding - minPadding) + minPadding);
+        } else {
+          padding = maxPadding;
+        }
+
+        var widthValue = ( dummy.offsetWidth + padding) + 'px';
+        input.setAttribute('style', 'width:' + widthValue);
+        input.style.width = widthValue;
+      }
+    }
+
+    // Any time the input changes, or the window resizes, adjust the size of the input box
+    input.addEventListener('input', adjustInput);
+    window.addEventListener('resize', adjustInput);
+
+    // Trigger the input event once to set up the input box and dummy element
     Common.fireEvent(input, 'input');
   }
 
