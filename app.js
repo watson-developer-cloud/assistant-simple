bfd9cd2942e5a842c86d7c0c08830e2268966a5e
Nathan Friedly <nfriedly@users.noreply.github.com>
2017-05-09 11:40:21 -0400

    Removing unnecessary URL and version params
    The conversationV1 constructor defaults to the provided US url, and the version is already specified in the require path (line 21). 
    
    Also, hard-coding the URL means this demo won't work without modification in other bluemix regions, because it overrides the region-specific one provided in VCAP_SERVICES.

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ var conversation = new Conversation({
   // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
   // username: '<username>',
   // password: '<password>',
-  url: 'https://gateway.watsonplatform.net/conversation/api',
   version_date: '2016-10-21',
-  version: 'v1'
 });
 
 // Endpoint to be call from the client side
