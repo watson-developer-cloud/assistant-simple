7991b08e961e47f52bcf5de6e80a652d61d3c6ed
Tomas Macek <tomas_macek@cz.ibm.com>
2017-08-24 12:42:24 +0200

    Conversation API version upgraded to VERSION_DATE_2017_05_2

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ var conversation = new Conversation({
   // username: '<username>',
   // password: '<password>',
   // url: 'https://gateway.watsonplatform.net/conversation/api',
-  version_date: Conversation.VERSION_DATE_2017_04_21
-});
+//  version_date: Conversation.VERSION_DATE_2017_04_21
+   version_date: Conversation.VERSION_DATE_2017_05_26
+  });
 
 // Endpoint to be call from the client side
 app.post('/api/message', function(req, res) {
