e93be6b28de32cac34c779f9019cee0b68da718a
Erika Dsouza <erika27desouza@gmail.com>
2018-03-15 17:59:10 -0400

    fix(eslint error):

--- test/integration/test.mainpage.js
+++ test/integration/test.mainpage.js
@@ -32,7 +32,7 @@ casper.test.begin('Watson Assistant simple Demo', 5, function suite(test) {
     casper.waitForSelector('.from-user', function () {
       test.assertExists('.message-inner', 'Message sent');
       test.assertTextExists('turn the wipers on', 'Message in bubble');
-      casper.waitForText("I'll turn on the wipers for you.");
+      casper.waitForText('I\'ll turn on the wipers for you.');
     });
   }
 
