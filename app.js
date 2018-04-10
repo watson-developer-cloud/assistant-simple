ac6d327723cec1510d5c3ac2088f41835e62fedf
Steve Green <stevenpkg@au1.ibm.com>
2018-04-10 10:44:08 +1000

    Update watson-developer-cloud to 3.2.1 app.js now sets up an Assistant service instead of Conversation

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ app.use(bodyParser.json());
 
 // Create the service wrapper
 
-var conversation = new watson.ConversationV1({
+var conversation = new watson.AssistantV1({
   // If unspecified here, the ASSISTANT_USERNAME and ASSISTANT_PASSWORD env properties will be checked
   // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
   username: process.env.ASSISTANT_USERNAME || '<username>',
   password: process.env.ASSISTANT_PASSWORD || '<password>',
-  version_date: '2018-02-16'
+  version: '2018-02-16'
 });
 
 // Endpoint to be call from the client side
